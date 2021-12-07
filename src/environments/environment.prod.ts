ba0ac1152c5c3ec16b6511e30a2c63347ebccec3
tobey10 <fafowora.oluwatobiloba@academicianhelp.com>
2021-12-07 14:25:37 +0100

    update fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  stripeTextKey: 'pk_test_51JzcYECj0GByp4pJT59pqXNjpKLlSc3OSbV6QZoh5YZJ0OoWn5oSdT4hVYoOhDc31WC1RFttvqO2LbMHsqiSJYXb00p4EiZJXn'
 };
